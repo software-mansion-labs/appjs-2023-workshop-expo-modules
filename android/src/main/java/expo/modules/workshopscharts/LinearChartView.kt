3076c1d8f94b6b655f52cd754d982814e3a5b808
lukmccall <kosmatylukasz@gmail.com>
2023-05-09 16:31:42 +0200

    passing-data-to-view-task-1

--- android/src/main/java/expo/modules/workshopscharts/LinearChartView.kt
+++ android/src/main/java/expo/modules/workshopscharts/LinearChartView.kt
@@ -1,10 +1,23 @@
 package expo.modules.workshopscharts
 
 import android.content.Context
+import android.view.ViewGroup
+import com.github.mikephil.charting.charts.LineChart
 import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.views.ExpoView
+import expo.modules.workshopscharts.Utils.applyDefaultSettings
 
 class LinearChartView(
   context: Context,
   appContext: AppContext
-) : ExpoView(context, appContext)
+) : ExpoView(context, appContext) {
+  private val chartView = LineChart(context)
+
+  init {
+    chartView.applyDefaultSettings()
+
+    addView(chartView, ViewGroup.LayoutParams(
+      ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT
+    ))
+  }
+}
--- ios/LinearChartView.swift
+++ ios/LinearChartView.swift
@@ -2,7 +2,13 @@ import ExpoModulesCore
 import Charts
 
 class LinearChartView: ExpoView {
+  let chartView = LineChartView(frame: .zero)
+
   public required init(appContext: AppContext? = nil) {
     super.init(appContext: appContext)
+
+    chartView.applyDefaultSettings()
+
+    addSubview(chartView)
   }
 }
